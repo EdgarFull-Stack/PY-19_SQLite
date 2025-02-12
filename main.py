502906c7fcdfd1c113459a7f63f477dca3f84eaa
Edgar Lipnevic <edgarlipnevic@gmail.com>
2025-02-12 20:56:48 +0200

    Task 2-3 add

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
 import sqlite3
+#
+# conn = sqlite3.connect('pavyzdys.db')
+# c = conn.cursor()
+#
+# c.execute(
+#     '''
+#     CREATE TABLE IF NOT EXISTS studentai (
+#                 vardas TEXT,
+#                 pavarde TEXT,
+#                 klase INTEGER)
+#     '''
+# )
+#
+# conn.commit()
+# conn.close()
 
-conn = sqlite3.connect('pavyzdys.db')
-c = conn.cursor()
+def append_to_studentai(vardas, pavarde, klase):
+    with sqlite3.connect('pavyzdys.db') as conn:
+        c = conn.cursor()
+        c.execute('INSERT INTO studentai (vardas, pavarde, klase) VALUES (?,?,?)',(vardas, pavarde, klase))
+def print_all_studentai_rows():
+    with sqlite3.connect('pavyzdys.db') as conn:
+        c = conn.cursor()
+        for row in c.execute('SELECT * FROM studentai'):
+            print(row)
 
-c.execute(
-    '''
-    CREATE TABLE IF NOT EXISTS studentai (
-                vardas TEXT,
-                pavarde TEXT,
-                klase INTEGER)
-    '''
-)
+def print_all_studentai_names():
+    with sqlite3.connect('pavyzdys.db') as conn:
+        c = conn.cursor()
+        for row in c.execute('SELECT vardas FROM studentai'):
+            print(row)
 
-conn.commit()
-conn.close()
+def print_all_studentai_by_klase(klase):
+    with sqlite3.connect('pavyzdys.db') as conn:
+        c = conn.cursor()
+        for row in c.execute('SELECT * FROM studentai WHERE klase = ?',(klase,)):
+            print(row)
+
+# append_to_studentai('Edgar', 'Lip',12)
+# append_to_studentai('John', 'John',10)
+print_all_studentai_rows()
+print_all_studentai_names()
+print_all_studentai_by_klase(10)
--- tasks.py
+++ tasks.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+# Task 1
 conn = sqlite3.connect('mokykla.db')
 c = conn.cursor()
 
@@ -14,3 +14,29 @@ c.execute(
 
 conn.commit()
 conn.close()
+print('-'*40)
+# Task 2
+def append_to_mokykla(pavadinimas, adresas, mokiniu_skaicius):
+    with sqlite3.connect('mokykla.db') as conn:
+        c = conn.cursor()
+        c.execute('INSERT INTO mokykla (pavadinimas, adresas, mokiniu_skaicius) VALUES (?,?,?)',(pavadinimas, adresas, mokiniu_skaicius))
+
+# append_to_mokykla("Vilniausprogimnazija","Vilniausg.10",500)
+# append_to_mokykla("Kaunogimnazija","Kaunog.5",800)
+# Task 3
+def print_all_mokykla_rows():
+    with sqlite3.connect('mokykla.db') as conn:
+        c = conn.cursor()
+        for row in c.execute('SELECT * FROM mokykla'):
+            str(row)
+            print(f'Mokykla: {row[0]}, Adresas: {row[1]}, Mokiniu skaicius: {row[2]}')
+
+print_all_mokykla_rows()
+
+
+def print_mokykla_by_skaicius(mokiniu_skaicius):
+    with sqlite3.connect('mokykla.db') as conn:
+        c = conn.cursor()
+        for row in c.execute('SELECT * FROM mokykla WHERE mokiniu_skaicius >= ?',(mokiniu_skaicius,)):
+            print(row)
+print(print_mokykla_by_skaicius(600))
